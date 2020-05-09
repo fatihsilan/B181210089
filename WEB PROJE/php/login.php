fbdb2116568e9dee8467ba92c34d6378a9548e81
fatihsilan <62845685+fatihsilan@users.noreply.github.com>
2020-05-09 21:11:34 +0300

    COMMIT 3
    BOOSTRAP RESPONSIVE

--- WEB PROJE/php/login.php
+++ WEB PROJE/php/login.php
@@ -1,50 +1,24 @@
 <!DOCTYPE html>
-<html>
+<html >
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>B181210089</title>
-    <meta name="keywords" content="Hakkında_Anasayfa">
-    <meta name="description" content="Hakkında_Anasayfa">
-
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-    <link rel="stylesheet" href="css/global.css">
-    <link rel="stylesheet" href="css/main.css">
+    <title>Giriş Yap</title>
 </head>
 
 <body>
-    
-    <table cellspacing="5">
-        
-    
-    <?php
-            if(isset ($_POST["password" =="123"]))
-            ?>
-            {
-                <tr>
-                <td>HOŞGELDİNİZ</td>
-                <td></td>
-                <td>
-                
-                <?php
-                echo $_POST["email"];
-                echo "&nbsp";
-                ?>
-                </td>
-            </tr>
+<?php
+     $password = $_POST['password'];
+
+        if($password == "7355608")
+        {
+
+            echo "hoşgeldiniz";
+            echo $_POST["email"];
+            echo "&nbsp";
             
-			}
-    
-    
-        
-        
+        }
 
-
-
-    </section>
-
-
-
+    ?>
 </body>
 </html>
