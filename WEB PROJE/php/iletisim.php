891e4866993a6416ac11648ed0157abe5d2d8c65
fatihsilan <62845685+fatihsilan@users.noreply.github.com>
2020-05-10 07:21:58 +0300

    COMMIT 4
    Site Çalışan - Final

--- WEB PROJE/php/iletisim.php
+++ WEB PROJE/php/iletisim.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Giriş Yap</title>
+    <title>Bilgileriniz</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/loginphp.css">
+    <link rel="stylesheet" href="../css/iletisimphp.css">
 </head>
 
 <body>
@@ -45,36 +45,33 @@
         </nav>
     </div>
 
-    <div class="font-weight-bold p-5 bg-success justify-content-center">
-
-    <br>
-
+    <div class="font-weight-bold p-5 bg-success justify-content-center"  >
     <?php
-            $password = $_POST['password'];
 
-                if($password == "7355608")
-                    {
+        $gelenAd = $_POST["name"];
+        $gelenSoyad = $_POST["surname"];
+        $gelenMail = $_POST["email"];
+        $gelenCinsiyet = $_POST["gender"];
+        $gelenHobi = $_POST["hobi"];
+        $gelenEgitim = $_POST["egitim"];
+        $gelenMesaj = $_POST["message"];
 
-                        echo "HOŞGELDİNİZ" . "<br/>";
+        echo "Adınız : " . $gelenAd . "<br />";
+        echo "Soyadınız : " . $gelenSoyad . "<br />";
+        echo "Mail Adresiniz : " . $gelenMail . "<br />";
+        echo "Cinsiyetiniz : " . $gelenCinsiyet . "<br />";
 
-                        echo "&nbsp";
+        echo "Hobileriniz : ";
+        foreach($gelenHobi as $Bilgiler){
+        echo $Bilgiler . "<br />";
+        }
 
-                        echo $_POST["email"];
+        echo "Eğitim Durumunuz : " . $gelenEgitim . "<br />";
+        echo "Mesajınız : " . $gelenMesaj . "<br />";
 
-                        
-            
-                    }
-
-                else
-                    {
-                        header("Location: ../login.html");
-                    }
-
-        ?>
-
+    ?>
     </div>
-        
     
-
 </body>
+
 </html>
